cfae7a7b09b42d74ac1552239ca0f277b4b38c9a
Samuel <samuel@innovega.net>
2012-12-21 13:46:10 +0100

    product association added to slide

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -2,4 +2,13 @@ class Spree::Slide < ActiveRecord::Base
   has_attached_file :image
   scope :published, where(:published => true)
   attr_accessible :name, :body, :link_url, :published, :image, :position, :product_id
+  belongs_to :product
+
+  def slide_name
+    name.blank? && product.present? ? product.name : name
+  end
+
+  def slide_link
+    link_url.blank? && product.present? ? product : link_url
+  end
 end
