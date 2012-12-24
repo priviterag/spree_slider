2512f80efa3db6811786a3827efe5f5ce655ea24
Samuel <samuel@innovega.net>
2012-12-24 13:31:15 +0100

    slide product views

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -4,11 +4,20 @@ class Spree::Slide < ActiveRecord::Base
   attr_accessible :name, :body, :link_url, :published, :image, :position, :product_id
   belongs_to :product
 
+  def initialize(attrs = nil)
+    attrs ||= {:published => true}
+    super
+  end
+
   def slide_name
     name.blank? && product.present? ? product.name : name
   end
 
   def slide_link
     link_url.blank? && product.present? ? product : link_url
+  end
+
+  def slide_image
+    !image.file? && product.present? && product.images.any? ? product.images.first.attachment : image
   end
 end
