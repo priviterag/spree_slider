6fb05cdea1d774b8405799c67ee9ff27f05826eb
Gerald Mueller <gerald.mueller@factor4.at>
2015-04-07 10:14:36 +0200

    Added :url, :path and :convert_options to Spree::Slide image attachment.

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,6 +1,9 @@
 class Spree::Slide < ActiveRecord::Base
 
-  has_attached_file :image
+  has_attached_file :image,
+                    url: '/spree/slides/:id/:style/:basename.:extension',
+                    path: ':rails_root/public/spree/slides/:id/:style/:basename.:extension',
+                    convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   scope :published, -> { where(published: true).order('position ASC') }
 
