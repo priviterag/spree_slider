e15ff02475012aa6d0b3c0b3e59e557dd0a7381e
Mafi <michal.markiewicz@sparksolutions.co>
2016-02-25 15:46:10 +0100

    Use new ruby hashes

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,7 +1,7 @@
 class Spree::Slide < ActiveRecord::Base
 
-  has_and_belongs_to_many :slide_locations, 
-                          class_name: 'Spree::SlideLocation', 
+  has_and_belongs_to_many :slide_locations,
+                          class_name: 'Spree::SlideLocation',
                           join_table: 'spree_slide_slide_locations'
 
   has_attached_file :image,
@@ -9,14 +9,14 @@ class Spree::Slide < ActiveRecord::Base
                     path: ':rails_root/public/spree/slides/:id/:style/:basename.:extension',
                     convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-  
+
   scope :published, -> { where(published: true).order('position ASC') }
-  scope :location, -> (location){ joins(:slide_locations).where('spree_slide_locations.name = ?', location) }
+  scope :location, -> (location) { joins(:slide_locations).where('spree_slide_locations.name = ?', location) }
 
   belongs_to :product, touch: true
 
   def initialize(attrs = nil)
-    attrs ||= {:published => true}
+    attrs ||= { published: true }
     super
   end
 
